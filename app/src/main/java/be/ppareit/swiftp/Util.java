1a79ab3a8e4f209a9cfe9b33f4e8b4ccdf63325c
crazyworldzl <allen.zl@outlook.com>
2017-05-08 09:24:59 +0800

    Initial commit

---
+++ app/src/main/java/be/ppareit/swiftp/Util.java
@@ -0,0 +1,216 @@
+/*
+Copyright 2011-2013 Pieter Pareit
+Copyright 2009 David Revell
+
+This file is part of SwiFTP.
+
+SwiFTP is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SwiFTP is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SwiFTP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package be.ppareit.swiftp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+abstract public class Util {
+    final static String TAG = Util.class.getSimpleName();
+
+    public static byte byteOfInt(int value, int which) {
+        int shift = which * 8;
+        return (byte) (value >> shift);
+    }
+
+    public static String ipToString(int addr, String sep) {
+        // myLog.l(Log.DEBUG, "IP as int: " + addr);
+        if (addr > 0) {
+            StringBuffer buf = new StringBuffer();
+            buf.append(byteOfInt(addr, 0)).append(sep).append(byteOfInt(addr, 1))
+                    .append(sep).append(byteOfInt(addr, 2)).append(sep)
+                    .append(byteOfInt(addr, 3));
+            Log.d(TAG, "ipToString returning: " + buf.toString());
+            return buf.toString();
+        } else {
+            return null;
+        }
+    }
+
+    public static InetAddress intToInet(int value) {
+        byte[] bytes = new byte[4];
+        for (int i = 0; i < 4; i++) {
+            bytes[i] = byteOfInt(value, i);
+        }
+        try {
+            return InetAddress.getByAddress(bytes);
+        } catch (UnknownHostException e) {
+            // This only happens if the byte array has a bad length
+            return null;
+        }
+    }
+
+    public static String ipToString(int addr) {
+        if (addr == 0) {
+            // This can only occur due to an error, we shouldn't blindly
+            // convert 0 to string.
+            Log.e(TAG, "ipToString won't convert value 0");
+            return null;
+        }
+        return ipToString(addr, ".");
+    }
+
+    public static String[] concatStrArrays(String[] a1, String[] a2) {
+        String[] retArr = new String[a1.length + a2.length];
+        System.arraycopy(a1, 0, retArr, 0, a1.length);
+        System.arraycopy(a2, 0, retArr, a1.length, a2.length);
+        return retArr;
+    }
+
+    public static void sleepIgnoreInterrupt(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            /* swallow */
+        }
+    }
+
+    /**
+     * Creates a SimpleDateFormat in the formatting used by ftp sever/client.
+     */
+    private static SimpleDateFormat createSimpleDateFormat() {
+        SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss", Locale.US);
+        df.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return df;
+    }
+
+    public static String getFtpDate(long time) {
+        SimpleDateFormat df = createSimpleDateFormat();
+        return df.format(new Date(time));
+    }
+
+    public static Date parseDate(String time) throws ParseException {
+        SimpleDateFormat df = createSimpleDateFormat();
+        return df.parse(time);
+    }
+    public static void openFile(Context context, File file) {
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //设置intent的Action属性
+        intent.setAction(Intent.ACTION_VIEW);
+        //获取文件file的MIME类型
+        String type = getMIMEType(file);
+        //设置intent的data和Type属性。
+        intent.setDataAndType(Uri.fromFile(file), type);
+        //跳转
+        try {
+            context.startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(context, "找不到打开此文件的应用！", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static String getMIMEType(File file) {
+        String type = "*/*";
+        String fName = file.getName();
+        //获取后缀名前的分隔符"."在fName中的位置。
+        int dotIndex = fName.lastIndexOf(".");
+        if (dotIndex < 0) {
+            return type;
+        }
+        /* 获取文件的后缀名*/
+        String end = fName.substring(dotIndex, fName.length()).toLowerCase();
+        if (end == "") return type;
+        //在MIME和文件类型的匹配表中找到对应的MIME类型。
+        for (int i = 0; i < MIME_MapTable.length; i++) { //MIME_MapTable??在这里你一定有疑问，这个MIME_MapTable是什么？
+            if (end.equals(MIME_MapTable[i][0]))
+                type = MIME_MapTable[i][1];
+        }
+        return type;
+    }
+
+    private static final String[][] MIME_MapTable = {
+            // {后缀名，MIME类型}
+            {".3gp", "video/*"},
+            {".apk", "application/vnd.android.package-archive"},
+            {".asf", "video/x-ms-asf"},
+            {".avi", "video/*"},
+            {".bin", "application/octet-stream"},
+            {".bmp", "image/bmp"},
+            {".c", "text/plain"},
+            {".class", "application/octet-stream"},
+            {".conf", "text/plain"},
+            {".cpp", "text/plain"},
+            {".doc", "application/msword"},
+            {".docx",
+                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+            {".xls", "application/vnd.ms-excel"},
+            {".xlsx",
+                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+            {".exe", "application/octet-stream"},
+            {".gif", "image/gif"},
+            {".gtar", "application/x-gtar"},
+            {".gz", "application/x-gzip"},
+            {".h", "text/plain"},
+            {".htm", "text/html"},
+            {".html", "text/html"},
+            {".jar", "application/java-archive"},
+            {".java", "text/plain"},
+            {".jpeg", "image/*"},
+            {".jpg", "image/*"},
+            {".js", "application/x-javascript"},
+            {".log", "text/plain"},
+            {".m3u", "audio/x-mpegurl"},
+            {".m4a", "audio/mp4a-latm"},
+            {".m4b", "audio/mp4a-latm"},
+            {".m4p", "audio/mp4a-latm"},
+            {".m4u", "video/vnd.mpegurl"},
+            {".m4v", "video/x-m4v"},
+            {".mov", "video/quicktime"},
+            {".mp2", "audio/x-mpeg"},
+            {".mp3", "audio/*"},
+            {".mp4", "video/*"},
+            {".mpc", "application/vnd.mpohun.certificate"},
+            {".mpe", "video/mpeg"},
+            {".mpeg", "video/mpeg"},
+            {".mpg", "video/mpeg"},
+            {".mpg4", "video/mp4"},
+            {".mpga", "audio/mpeg"},
+            {".msg", "application/vnd.ms-outlook"},
+            {".ogg", "audio/ogg"},
+            {".pdf", "application/pdf"},
+            {".png", "image/*"},
+            {".pps", "application/vnd.ms-powerpoint"},
+            {".ppt", "application/vnd.ms-powerpoint"},
+            {".pptx",
+                    "application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+            {".prop", "text/plain"}, {".rc", "text/plain"},
+            {".rmvb", "audio/x-pn-realaudio"}, {".rtf", "application/rtf"},
+            {".sh", "text/plain"}, {".tar", "application/x-tar"},
+            {".tgz", "application/x-compressed"}, {".txt", "text/plain"},
+            {".wav", "audio/*"}, {".wma", "audio/x-ms-wma"},
+            {".wmv", "audio/*"},
+            {".wps", "application/vnd.ms-works"}, {".xml", "text/plain"},
+            {".z", "application/x-compress"},
+            {".zip", "application/x-zip-compressed"}, {"", "*/*"}};
+}
---
+++ app/src/main/java/be/ppareit/swiftp/gui/BrowseFileActivity.java
@@ -0,0 +1,189 @@
+package be.ppareit.swiftp.gui;
+
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.CheckedTextView;
+import android.widget.GridView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import be.ppareit.swiftp.FsSettings;
+import be.ppareit.swiftp.R;
+import be.ppareit.swiftp.Util;
+
+/**
+ * Created by ZhangLong on 2017/5/4.
+ */
+
+public class BrowseFileActivity extends AppCompatActivity {
+    private GridView gv;
+    private Adapter adapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.browsefile);
+        setTitle(R.string.browse_file);
+        initView();
+    }
+
+    public void initView() {
+        gv = (GridView) findViewById(R.id.browsefile_gv);
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                File file = adapter.getFiles().get(position);
+                if (file.isDirectory()) {
+                    if (file.canRead()) {
+                        if (file.listFiles().length > 0) {
+                            List<File> files = Arrays.asList(file.listFiles());
+                            adapter.addFiles(files);
+                        } else {
+                            adapter.addEmptyFile(file);
+                        }
+                    }
+                } else {
+                    Util.openFile(view.getContext(), file);
+                }
+
+            }
+        });
+        adapter = new Adapter();
+        gv.setAdapter(adapter);
+        File chrootDir = null;
+        try {
+            chrootDir = new File(FsSettings.getChrootDir().getCanonicalPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<File> files = Arrays.asList(chrootDir.listFiles());
+        adapter.addFiles(files);
+    }
+
+
+}
+
+class Adapter extends BaseAdapter {
+    List<File> fileList;
+    List<Boolean> booleanList;
+
+    public Adapter() {
+        fileList = new ArrayList<File>();
+        booleanList = new ArrayList<Boolean>();
+    }
+
+    public void addFiles(List<File> files) {
+        File parent = files.get(0).getParentFile();
+        if (parent.getParentFile() != null) {
+            fileList.clear();
+            fileList.add(parent.getParentFile());
+            fileList.addAll(files);
+            notifyDataSetChanged();
+        }
+    }
+
+    public void addEmptyFile(File file) {
+        if (file.getParentFile() != null) {
+            fileList.clear();
+            fileList.add(file.getParentFile());
+            notifyDataSetChanged();
+        }
+    }
+
+    public void addFile(File file) {
+        fileList.add(file);
+        notifyDataSetChanged();
+    }
+
+    public List<File> getFiles() {
+        return fileList;
+    }
+
+    @Override
+    public int getCount() {
+        return fileList.size();
+    }
+
+    @Override
+    public File getItem(int position) {
+        return fileList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public void notifyDataSetChanged() {
+        booleanList = new ArrayList<>(fileList.size());
+        super.notifyDataSetChanged();
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (convertView == null) {
+            convertView = new CheckedTextView(parent.getContext());
+            holder = new ViewHolder();
+            holder.ctv = (CheckedTextView) convertView;
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(-1, -1);
+        layoutParams.width = parent.getWidth() * 10 / 31;
+        layoutParams.height = parent.getWidth() / 4;
+        holder.ctv.setLayoutParams(layoutParams);
+        holder.ctv.setBackgroundResource(R.drawable.browsefile_itembg);
+        holder.ctv.setTextColor(0xff000000);
+        holder.ctv.setPadding(5, 0, 5, 0);
+        if (position == 0) {
+            holder.ctv.setText("返回上一级");
+        } else {
+            holder.ctv.setText(fileList.get(position).getName());
+        }
+        Drawable drawable = null;
+        if (!fileList.get(position).isFile()) {
+            drawable = holder.ctv.getResources().getDrawable(R.mipmap.folder);
+        } else {
+            String mimeType = Util.getMIMEType(fileList.get(position));
+            if (mimeType.contains("video")) {
+                drawable = holder.ctv.getResources().getDrawable(R.mipmap.video);
+            } else if (mimeType.contains("image")) {
+                drawable = holder.ctv.getResources().getDrawable(R.mipmap.image);
+            } else if (mimeType.contains("audio")) {
+                drawable = holder.ctv.getResources().getDrawable(R.mipmap.audio);
+            } else if (mimeType.contains("txt")) {
+                drawable = holder.ctv.getResources().getDrawable(R.mipmap.txt);
+            } else {
+                drawable = holder.ctv.getResources().getDrawable(R.mipmap.unknow);
+            }
+        }
+        drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
+        holder.ctv.setCompoundDrawables(null, drawable, null, null);
+        holder.ctv.setGravity(Gravity.CENTER_HORIZONTAL);
+        holder.ctv.setPadding(0,30,0,0);
+        holder.ctv.setTextAlignment(CheckedTextView.TEXT_ALIGNMENT_CENTER);
+        return convertView;
+    }
+
+    class ViewHolder {
+        CheckedTextView ctv;
+    }
+}
